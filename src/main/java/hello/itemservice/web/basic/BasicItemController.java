da29c9940512012ca74cf37fd1fd32c7d794b02e
TaeHye0n <thk001229@gmail.com>
2023-07-18 15:28:11 +0900

    Feat: PRG Post/Redirect/Get

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -49,38 +49,38 @@ public class BasicItemController {
         itemRepository.save(item);
 
         model.addAttribute("item", item);
-
         return "basic/item";
     }
 
 //    @PostMapping("/add")
     public String addItemV2(@ModelAttribute("item") Item item) {
         itemRepository.save(item);
-
 //        model.addAttribute("item", item); //자동 추가 생략 가능
-
         return "basic/item";
     }
 
 //    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item ) { //()생략시 클래스의 첫글자를 소문자로 바꿔 추가됨
         itemRepository.save(item);
+        return "basic/item";
+    }
 
+//    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
         return "basic/item";
     }
 
     @PostMapping("/add")
-    public String addItemV4(Item item) {
+    public String addItemV5(Item item) {
         itemRepository.save(item);
-
-        return "basic/item";
+        return "redirect:/basic/items/" + item.getId();
     }
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-
         return "basic/editForm";
     }
 
